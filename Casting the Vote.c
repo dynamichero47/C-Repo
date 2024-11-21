028e2c6a753048a1adda5a45ec42090b466b4bff
dynamichero47 <dainubirudula@gmail.com>
2024-11-21 13:20:53 +0530

    Uploading code: Casting the Vote.c

---
+++ Casting the Vote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int X;
+    scanf("%d",&X);
+    if(X<=17)
+    {
+        printf("NO");
+    }
+    else
+    {
+        printf("YES");
+    }
+    return 0;
+}
